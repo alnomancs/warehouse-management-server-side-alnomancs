fa9c8474a813ed1fe5d38fbe090bd80058baa9be
alnomancs <alnoman.cs@gmail.com>
2022-05-08 02:30:39 +0100

    project initial

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5001;
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const res = require("express/lib/response");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v7yk2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
