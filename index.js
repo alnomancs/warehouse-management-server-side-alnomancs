2a1071cc4b37acb787e19abbc52ec50faca5022f
alnomancs <alnoman.cs@gmail.com>
2022-05-08 02:20:42 +0100

    make it better

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const res = require("express/lib/response");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v7yk2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    app.get("/products", async (req, res) => {
+      const productCollection = client.db("warehouse").collection("products");
+      const query = {};
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir());
+
 app.get("/", (req, res) => {
   res.send("warehouse management system");
 });
