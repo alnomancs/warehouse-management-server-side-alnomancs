6feea75ee22f4c701cd40efdb15565cd1f9aa6e6
alnomancs <alnoman.cs@gmail.com>
2022-05-08 02:53:29 +0100

    added hero

--- index.js
+++ index.js
@@ -38,6 +38,9 @@ run().catch(console.dir());
 app.get("/", (req, res) => {
   res.send("warehouse management system");
 });
+app.get("/hero", (req, res) => {
+  res.send("hero");
+});
 
 app.listen(port, () => {
   console.log(`Warehose management system running on ${port}`);
