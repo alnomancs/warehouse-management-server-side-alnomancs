51428515d147bebfd4dda53a414cf324d5bf91aa
alnomancs <alnoman.cs@gmail.com>
2022-05-09 16:47:27 +0100

    final checking

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function run() {
     await client.connect();
     const productCollection = client.db("warehouse").collection("products");
 
-    //my
+    //my item
     app.get("/products/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
